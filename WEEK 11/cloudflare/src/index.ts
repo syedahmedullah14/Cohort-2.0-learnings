8acb3e4b1bf7279dd3630635f80b9fcd436ad3f2
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-04 09:48:45 +0530

    intro to hono library

--- WEEK 11/cloudflare/src/index.ts
+++ WEEK 11/cloudflare/src/index.ts
@@ -11,8 +11,26 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// };
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.body);
+		console.log(request.headers);
+		console.log(request.method)
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
 };
