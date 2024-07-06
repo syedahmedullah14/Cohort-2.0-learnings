dd1d4811ea10bddb0c87182e7ac6f7c688acfb11
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-06 23:53:50 +0530

    Building a simple landing page usinf react-router

--- Week 7 Revision/src/App.jsx
+++ Week 7 Revision/src/App.jsx
@@ -2,17 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Landing from './Landing'
+import Dashbaord from './Dashbaord'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        Contex Api and prop drilling
-      </div>
-    </>
+  <BrowserRouter>
+
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/dashboard'>Dashboard</Link>
+        </li>
+      </ul>
+    </nav>
+
+      <Routes>
+        <Route path='/' element={<Landing />}/>
+        <Route path='/dashboard' element={<Dashbaord />}/>
+      </Routes>
+  </BrowserRouter>
   )
 }
 
