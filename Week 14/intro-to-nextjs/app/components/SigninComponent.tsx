6b935d4cf8449d875e6c08ab67a35228961583ce
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-08 04:53:59 +0530

    Differing client components as much as possible

--- Week 14/intro-to-nextjs/app/components/SigninComponent.tsx
+++ Week 14/intro-to-nextjs/app/components/SigninComponent.tsx
@@ -1,9 +1,6 @@
-'use client'
-export default function SigninComponent() {
+import { Button } from "./Button";
 
-    function handler() {
-        console.log("hi")
-    }
+export default function SigninComponent() {
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
         <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
@@ -16,7 +13,7 @@ export default function SigninComponent() {
                     <div className="pt-2">
                         <LabelledInput label="Username" placeholder="syedjaser@gmail.com" />
                         <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                        <button onClick={handler} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+                        <Button />
                     </div>
                 </div>
             </a>
