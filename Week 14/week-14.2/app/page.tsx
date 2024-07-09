7311655bbf77d4ee2d79adc27e80a869ec0a8b81
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-09 06:42:21 +0530

    Adding a loader in Nexj with 'loading.tsx'

--- Week 14/week-14.2/app/page.tsx
+++ Week 14/week-14.2/app/page.tsx
@@ -1,19 +1,25 @@
 import axios from "axios";
 
 async function getUserDetails() {
+  await new Promise((r) => setTimeout(r, 5000))
   const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
 	return response.data;
 }
 
-//async component
 export default async function Home() {
   const userData = await getUserDetails();
 
   return (
-    <div className="flex justify-center border-b rounded-sm p-5 w-fit bg-black text-cyan-600">
-      hi there <br />
-      {userData.email}
-      {userData.name}
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
     </div>
   );
 }
