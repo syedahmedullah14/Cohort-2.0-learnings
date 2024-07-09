86cb4ede8b03154ed4f82b386d2b2ee6ec5d0320
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-09 09:36:08 +0530

    POST and PUT routes in Nextjs

--- Week 14/week-14.2/app/api/user/route.ts
+++ Week 14/week-14.2/app/api/user/route.ts
@@ -1,9 +1,31 @@
 export function GET(){
-
     //database logic
-
     return Response.json({
         username: "jaser@gmail.com",
         name: "Jaser"
     })
 }
+
+export function POST(){
+    //database logic
+    return Response.json({
+        username: "jaser@gmail.com",
+        name: "Jaser"
+    })
+}
+
+export function PUT(){
+    //database logic
+    return Response.json({
+        username: "jaser@gmail.com",
+        name: "Jaser"
+    })
+}
+
+// how would been written in express.js
+// app.get('/api/user', (res, req) => {
+//     res.json({
+//         username: "jaser@gmail.com",
+//         name: "jaser"
+//     })
+// })
