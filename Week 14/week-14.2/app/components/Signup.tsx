0ebfb000f3e744e831cad42ed608b264b7690835
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-10 01:35:02 +0530

    useRouter in Nextjs

--- Week 14/week-14.2/app/components/Signup.tsx
+++ Week 14/week-14.2/app/components/Signup.tsx
@@ -2,10 +2,12 @@
 
 import axios from "axios";
 import { ChangeEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const router = useRouter()
 
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
@@ -28,7 +30,7 @@ export function Signup() {
                                 username,
                                 password
                             });
-                            // router.push("/")
+                            router.push("/")
                         }} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign up</button>
                     </div>
                 </div>
